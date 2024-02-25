02ac551327834d4e92be3a58594c5a693522ce46
JosemiGT <JM.GamarroTornay@Outlook.com>
2024-02-25 12:39:24 +0100

    unocafe app exercises

--- part1/unicafe-app/src/App.jsx
+++ part1/unicafe-app/src/App.jsx
@@ -4,6 +4,41 @@ const Button = ({handleButton, text}) => {
   return (<button onClick={handleButton}> {text} </button>)
 }
 
+const StatisticsLine = ({statisticText, staticsValue}) => {
+  return (
+    <tr>
+      <th scope="row">{statisticText}</th>
+      <td>{staticsValue}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  return(
+    
+    <>
+      <thead>
+      <tr>
+        <th scope="col">Statistics type</th>
+        <th scope="col">Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <StatisticsLine statisticText="good" staticsValue={good}></StatisticsLine>
+      <StatisticsLine statisticText="neutral" staticsValue={neutral}></StatisticsLine>
+      <StatisticsLine statisticText="bad" staticsValue={bad}></StatisticsLine>
+      <StatisticsLine statisticText="all" staticsValue={(good+neutral+bad)}></StatisticsLine>
+      <StatisticsLine statisticText="average" staticsValue={(good*1+neutral*0+bad*-1)/3}></StatisticsLine>
+      <StatisticsLine statisticText="positive" staticsValue={(good)/(good+neutral+bad)}></StatisticsLine>
+    </tbody>
+      </>
+  )
+}
+
+const handleButton = (setValue, value) => {
+  setValue(value +1 )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
@@ -13,14 +48,14 @@ const App = () => {
   return (
     <>
       <h1>give feedback</h1>
-      <Button handleButton={() => (setGood(good + 1))} text="good"></Button>
-      <Button handleButton={() => (setNeutral(neutral + 1))} text="neutral"></Button>
-      <Button handleButton={() => (setBad(bad + 1))} text="bad"></Button>
+      <Button handleButton={() => {handleButton(setGood, good)}} text="good"></Button>
+      <Button handleButton={() => {handleButton(setNeutral, neutral)}} text="neutral"></Button>
+      <Button handleButton={() => {handleButton(setBad, bad)}} text="bad"></Button>
 
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      {(good + neutral + bad > 0) 
+        ? (<Statistics good={good} neutral={neutral} bad={bad}></Statistics> )
+        : (<p>No feedback given</p> )}
     </>
   )
 }
